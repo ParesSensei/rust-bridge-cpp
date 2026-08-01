62945bb9c8323d29e764410bd1b5da7a0214b6ae
Pares <alfariz2004@gmail.com>
2026-08-01 15:40:05 +0700

    feat: trait exercise

--- src/exercise_trait.rs
+++ src/exercise_trait.rs
@@ -65,3 +65,55 @@ fn main1() {
     let result = person.say_hello_to("budi");
     println!("{}", result);
 }
+
+trait Animal {
+    // Default implementation
+    fn is_mammal(&self) -> bool {
+        true
+    }
+}
+trait Feline : Animal {
+    // Default implementation
+    fn is_feline(&self) -> bool {
+        true
+    }
+}
+
+struct Cat;
+// Use default implementations. Note that all traits for the supertrait must be individually implemented
+impl Feline for Cat {}
+impl Animal for Cat {}
+
+#[test]
+fn main2() {
+    let c = Cat{};
+    println!("{} {}", c.is_mammal(), c.is_feline());
+}
+
+trait Log{
+    fn log(&self, msg: u64);
+}
+
+struct SimpleLogger{}
+
+struct ComplexLogger{}
+
+impl Log for SimpleLogger{
+    fn log(&self, msg: u64){
+        println!("Simple logger : {}",  msg);
+    }
+}
+
+impl Log for ComplexLogger{
+    fn log(&self, msg: u64){
+        println!("Complex Logger : {} (hex: 0x{msg:x}, binary: {msg:b})", msg)
+    }
+}
+#[test]
+fn tes_log() {
+    let simple = SimpleLogger{};
+    simple.log(3);
+
+    let complex = ComplexLogger{};
+    complex.log(45);
+}
